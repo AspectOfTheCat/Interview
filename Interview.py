87704aeed2e007bd0d925447062b8f48533b1379
MBH369 <67119058+MBH369@users.noreply.github.com>
2020-06-18 16:35:36 -0400

    Create Interview.py

---
+++ Interview.py
@@ -0,0 +1,26 @@
+question1 = input("What is your favorite programming language?")
+
+if question1 == "Python":
+    print("Mine too!")
+    
+else:
+    print("Ok!")
+    
+    
+question2 = input("What is your favorite colour?")
+
+if question2 == "Blue":
+    print("Mine too!")
+
+else:
+    print("oK!")
+    
+question3 = input("Do you use github?")
+
+if question3 == "No":
+    print("Liar!")
+    
+else:
+    print(question1   +   question2  +  question3)
+    
+    
